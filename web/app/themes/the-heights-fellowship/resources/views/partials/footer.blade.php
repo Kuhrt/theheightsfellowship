0c414977ae91bc8b1160648c3dfed2f8878282f9
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-07-03 16:16:43 -0500

    Finished footer styles

--- web/app/themes/the-heights-fellowship/resources/views/partials/footer.blade.php
+++ web/app/themes/the-heights-fellowship/resources/views/partials/footer.blade.php
@@ -1,4 +1,14 @@
 <footer class="content-info">
   @php dynamic_sidebar('sidebar-footer') @endphp
-  <p class="copyright">&copy; The Heights Fellowship</p>
+  <a href="/" class="footer__logo">
+    <img src="@asset('images/thflogosquarewhite.svg')" alt="The Heights Fellowship logo white">
+  </a>
+  <div class="footer__social">
+    <a href="http://www.facebook.com/theheightsfellowship" class="footer-social__icon facebook"></a>
+    <a href="http://www.twitter.com/thflubbock" class="footer-social__icon twitter"></a>
+    <a href="http://instagram.com/thflubbock" class="footer-social__icon instagram"></a>
+    <a href="http://www.youtube.com/user/TheHeightsFellowship" class="footer-social__icon youtube"></a>
+    <a href="https://itunes.apple.com/us/app/heights-fellowship-lubbock/id424692506?mt=8" class="footer-social__icon itunes"></a>
+  </div>
+  <p class="copyright">&copy; The Heights Fellowship @php echo date('Y') @endphp</p>
 </footer>
