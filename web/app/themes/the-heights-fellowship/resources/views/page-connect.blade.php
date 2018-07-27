83bc2ddd0b97ee654a051462f06e407187dd610e
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-07-27 12:27:09 -0500

    Finished connect page styles

--- web/app/themes/the-heights-fellowship/resources/views/page-connect.blade.php
+++ web/app/themes/the-heights-fellowship/resources/views/page-connect.blade.php
@@ -8,8 +8,19 @@
     @endphp
     <section class="thf__connect">
       @while($loop->have_posts()) @php $loop->the_post() @endphp
-        <div class="thf-connect__page">
-        </div>
+        @php
+          $thumb_id = get_post_thumbnail_id();
+          $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
+          $thumb_url = $thumb_url_array[0];
+        @endphp
+        <a class="thf-connect__page" href="<?php the_permalink(); ?>">
+          <div class="thf-connect-page__image">
+            <div class="thf-connect-page-image__bg" style="background-image:url('@php echo $thumb_url @endphp');"></div>
+          </div>
+          <div class="thf-connect-page__title">
+            <h3><?php the_title(); ?></h3>
+          </div>
+        </a>
       @endwhile
     </section>
     @php wp_reset_query() @endphp
