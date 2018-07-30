32fcb23b8ae39d53244a8233f5624cb7c4e26ebd
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-07-30 16:59:24 -0500

    Started adding the secondary menu

--- web/app/themes/the-heights-fellowship/resources/views/partials/header.blade.php
+++ web/app/themes/the-heights-fellowship/resources/views/partials/header.blade.php
@@ -64,14 +64,23 @@
     </g>
     </svg>
   </a>
-  <nav class="nav-primary">
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+  <div class="nav-container">
+    <nav class="nav-primary">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+    </nav>
+    <div class="banner__hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    @if (has_nav_menu('secondary_navigation'))
+    <nav class="nav-secondary">
+      <p class="nav-secondary__title">Youth</p>
+      {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'nav']) !!}
+    </nav>
     @endif
-  </nav>
-  <div class="banner__hamburger">
-    <span></span>
-    <span></span>
-    <span></span>
   </div>
 </header>
