76fce08365b7170ed7e04f9bf65d37f74583cd98
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-08-01 15:23:03 -0500

    Styled login form

--- web/app/themes/the-heights-fellowship/app/admin.php
+++ web/app/themes/the-heights-fellowship/app/admin.php
@@ -22,3 +22,15 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * Login form
+ */
+add_action('login_enqueue_scripts', function () {
+   wp_enqueue_style(
+       'thf-login.css',
+       asset_path('images/login.css'),
+       false,
+       null
+   );
+}, 100);
