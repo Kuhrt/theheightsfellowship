f29b8fa015e3ea7e14ba5d293962f34d9bcd961e
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-08-08 11:38:42 -0500

    Added the content for the LIFEGroups page above the grid

--- web/app/themes/the-heights-fellowship/resources/views/lifegroups-template.blade.php
+++ web/app/themes/the-heights-fellowship/resources/views/lifegroups-template.blade.php
@@ -9,16 +9,23 @@
     @php
       $loop = new WP_Query( array( 'post_type' => 'lifegroup', 'posts_per_page' => -1 ) );
     @endphp
+    <section class="thf-life-groups__content">
+      @include('partials.content-page')
+    </section>
     <section class="thf__life-groups">
       @while($loop->have_posts()) @php $loop->the_post() @endphp
-        @php
-          $thumb_id = get_post_thumbnail_id();
-          $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
-          $thumb_url = $thumb_url_array[0];
-        @endphp
         <a class="thf-life-groups__group" href="<?php the_permalink(); ?>">
           <div class="thf-life-groups-group__image">
-            <div class="thf-life-groups-group-image__bg"></div>
+            @if (has_post_thumbnail())
+              @php
+                $thumb_id = get_post_thumbnail_id();
+                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'large', true);
+                $thumb_url = $thumb_url_array[0];
+              @endphp
+              <div class="thf-life-groups-group-image__bg" style="background-image:url('@php echo $thumb_url @endphp');"></div>
+            @else
+              <div class="thf-life-groups-group-image__bg"></div>
+            @endif
           </div>
           <div class="thf-life-groups-group__title">
             <h3><?php the_title(); ?></h3>
