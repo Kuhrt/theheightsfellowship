bc33549954e5ebd333c89e7a4f6bbd1d4cdc0a63
Kuhrt Cowan <kuhrt.cowan@ttu.edu>
2018-08-09 11:53:28 -0500

    Added single event template

---
+++ web/app/themes/the-heights-fellowship/resources/eventon/single-ajde_events.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <?php //do_action('eventon_before_main_content'); ?>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/icons/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="theme-color" content="#ffffff">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Open+Sans:400,400i,700,700i" rel="stylesheet">
+    <?php wp_head() ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <?php do_action('get_header'); ?>
+    <header class="banner">
+      <a class="brand" href="/">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" class="logo" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        	 width="50px" height="45.9px" viewBox="0 0 50 45.9" style="enable-background:new 0 0 50 45.9;" xml:space="preserve">
+        <g>
+        	<g>
+        		<path class="st0" d="M6.3,0C1.7,0,0,2.4,0,6.5v24.6c0.4-0.4,0.8-0.8,1.3-1.1c0.3-0.5,0.7-0.7,1.1-1.1c0.1,0,0.2,0,0.2-0.1
+        			c0,0,0-0.2,0.1-0.2c0.3-0.3,0.7-0.5,1-0.7c0.4-0.4,0.8-1,1.2-1.5c0.5-0.6,1.2-1.3,1.4-2.1c2-2.8,3.5-6,6.4-7.8
+        			c0.7,0.3,1.5,0.4,2.2,0.7c0.3,0.9,0.3,1.7,0.5,2.6c0.3,0.8,0.8,1.4,1.1,2.2c0.1,0.4,0.1,0.9,0.3,1.3c0.2,0,0.3-0.3,0.4-0.4
+        			c0.4-0.6,0.6-1.3,0.8-1.9c0.4-0.8,0.9-1.5,1.4-2.2c1.1-1.4,2.2-2.6,3.3-3.9c0.6-0.6,1-1.4,1.5-2.1c0.5-0.7,1.1-1.3,1.6-2
+        			c1-1.4,2.2-2.6,3.4-3.8V6.8h1.5c0.1,0.3,0.7,0.1,0.9,0.3c1.3,2.3,2.7,4.5,4,6.7c0.4,0.7,1,1.4,1.4,2.2c0.3,0.6,0.5,1.3,0.8,1.8
+        			s0.7,1,1.1,1.6c0.7,1.1,1.4,2.1,2.1,3.2c1.7,2.7,3.3,5.5,5.2,8c0.4,0.5,0.7,1.1,1.1,1.6c0.9,1.3,1.8,2.5,2.7,3.7v-13V0H6.3z"/>
+        		<path class="st0" d="M50,45.9v-7.8L49.9,38c-0.3-0.2-0.6-0.6-1-1c-0.4-0.3-0.8-0.6-1.1-0.9c-0.3-0.3-0.5-0.6-0.7-1
+        			c-0.3-0.3-0.5-0.7-0.8-1c-0.5-0.6-1.2-1.1-1.7-1.8c-3-3.6-5.4-8-8-11.9c-0.1-0.2-0.3-0.3-0.4-0.5c-0.1-0.3-0.1-0.6-0.3-0.9
+        			c-0.2-0.5-0.6-0.8-0.8-1.3c-0.1-0.2-0.2-0.5-0.3-0.7c-0.2-0.4-0.5-0.8-0.8-1.2c-0.8-1.2-1.4-2.5-2.3-3.6c0-0.2,0-0.3-0.1-0.4
+        			c-0.4-0.4-0.6-1-1.1-1.3c-2.6,2.2-4.8,5-7.1,7.9c-1.8,2.4-3.8,5.1-5.4,7.8c-0.2,0.4-0.5,1.1-0.7,1.2c-0.1,0-0.4-0.1-0.7-0.2
+        			c-0.7-0.2-1.5-0.4-2-0.6c-0.3-2.2-1.4-3.6-2.1-5.5c-0.8,0.8-1.6,1.7-2.2,2.5c-0.2,0.3-0.4,0.6-0.8,0.8c0-0.3,0.3-0.3,0.3-0.6
+        			c-0.3,0.1-0.6,0.2-0.7,0.5c-0.2,0.3,0,0.7-0.3,0.9c-0.2,0.1-0.3,0.1-0.5,0.2c-0.1,0.1-0.2,0.3-0.4,0.3c-0.1,0.8-0.8,1.1-1.2,1.6
+        			c-0.1,0.1-0.1,0.3-0.2,0.5s-0.2,0.3-0.3,0.4c-0.2,0.3-0.4,0.7-0.5,0.9c-0.2,0.2-0.4,0.2-0.6,0.4c-0.2,0.2-0.3,0.6-0.5,0.7
+        			c-0.2,0-0.1-0.3-0.3-0.2c-0.3,0.4-0.7,0.8-1.2,1c-0.6,0.5-1,1.2-1.6,1.7c-0.5,0.5-1,0.8-1.6,1.2v11.8C0,45.9,50,45.9,50,45.9z"/>
+        	</g>
+        </g>
+        </svg>
+        <svg version="1.1" id="Layer_1" class="logo-words" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        	 width="113.5px" height="46px" viewBox="0 0 113.5 46" style="enable-background:new 0 0 113.5 46;" xml:space="preserve">
+        <g>
+        	<path class="st0" d="M1.4,0h9.4v2.4h-4v11.2H4V2.4H0v-1C0,0.7,0.6,0,1.4,0z"/>
+        	<path class="st0" d="M13.5,0h1.4v5.1h5.6V0h2.8v13.6h-2.8v-6h-5.6v6h-2.8V1.5C12.1,0.7,12.7,0,13.5,0z"/>
+        	<path class="st0" d="M24.7,1.5c0-1,0.4-1.4,1.4-1.5h8.4v2.4h-7.1v2.9h5.3v0.9c0,1-0.4,1.4-1.4,1.4h-3.9v3.5h7.1v2.5h-9.8V1.5z"/>
+        </g>
+        <g>
+        	<path class="st0" d="M1.4,15.9h1.4V21h5.6v-5.1h2.8v13.6H8.4v-6H2.8v6H0V17.4C0,16.6,0.6,15.9,1.4,15.9z"/>
+        	<path class="st0" d="M12.6,17.4c0-1,0.4-1.4,1.4-1.5h8.4v2.4h-7.1v2.9h5.3v0.9c0,1-0.4,1.4-1.4,1.4h-3.9V27h7.1v2.5h-9.8L12.6,17.4
+        		L12.6,17.4z"/>
+        	<path class="st0" d="M23.8,17.4c0-1,0.4-1.5,1.4-1.5h1.4v13.6h-2.8V17.4z"/>
+        	<path class="st0" d="M34.2,15.7c1.6,0,3,0.4,4.1,1.2c1,0.8,1.6,2,1.7,3.6h-1.7c-0.5,0-0.9-0.3-1.2-0.7c-0.5-1.1-1.5-1.6-2.8-1.6
+        		c-2.3,0-3.5,1.5-3.6,4.5c0,3,1.2,4.6,3.7,4.8c1.9,0,3-1.3,3.3-2.9H36c-0.8,0-1.4-0.6-1.4-1.4v-0.9h5.5v7.3h-0.7
+        		c-0.7,0-1.3-0.4-1.3-1.1L38,27.8c-1,1.2-1.9,1.9-3.9,1.9c-3.9,0-6.3-3-6.3-7C27.8,18.7,30.2,15.5,34.2,15.7z"/>
+        	<path class="st0" d="M42.9,15.9h1.4V21h5.6v-5.1h2.8v13.6h-2.8v-6h-5.6v6h-2.8V17.4C41.5,16.6,42.1,15.9,42.9,15.9z"/>
+        	<path class="st0" d="M55.4,15.9h9.4v2.4h-4v11.2H58V18.3h-4v-1C54,16.6,54.6,15.9,55.4,15.9z"/>
+        	<path class="st0" d="M74.4,25.7c0-0.7-0.4-1.1-1.2-1.3c-0.8-0.3-1.7-0.5-2.7-0.7c-2.7-0.7-4-1.9-4-3.7c0.1-2.9,1.8-4.3,5.1-4.2
+        		c3.2,0,4.9,1.4,5,4.2h-1.6c-0.6,0-1-0.3-1.2-0.8c-0.4-0.8-1.3-1.1-2.5-1.1c-1.4,0-2.2,0.6-2.2,1.6c0,0.9,0.9,1.5,2.6,1.9
+        		c1,0.3,2.1,0.5,3.2,1c1.5,0.5,2.2,1.6,2.2,3.4c-0.2,2.7-2,4-5.4,4c-3.6,0-5.4-1.5-5.5-4.5H69c0.1,1.4,1,2.2,2.8,2.2
+        		C73.6,27.4,74.4,26.9,74.4,25.7z"/>
+        </g>
+        <g>
+        	<path class="st0" d="M6.5,39.8H2.7v6H0V33.7c0-0.8,0.6-1.5,1.4-1.5h8.4v2.4H2.7v2.9H8v0.9C8,39.2,7.3,39.8,6.5,39.8z"/>
+        	<path class="st0" d="M11,33.7c0-0.9,0.4-1.4,1.4-1.5h8.4v2.4h-7.1v2.9H19v0.9c0,0.9-0.4,1.4-1.4,1.4h-3.8v3.5h7v2.5H11V33.7
+        		L11,33.7z"/>
+        	<path class="st0" d="M23.5,32.2h1.4v11.1h6.6v2.5h-9.4V33.7C22.1,32.9,22.7,32.2,23.5,32.2z"/>
+        	<path class="st0" d="M34,32.2h1.4v11.1H42v2.5h-9.3V33.7C32.6,32.9,33.3,32.2,34,32.2z"/>
+        	<path class="st0" d="M43,39c0-4.2,2.4-7,6.5-7c4,0,6.5,2.9,6.5,7c0,4.2-2.4,7-6.5,7C45.4,46,43,43.2,43,39z M45.8,39
+        		c0,2.7,1.2,4.7,3.7,4.7s3.7-1.9,3.7-4.7c0-2.7-1.2-4.6-3.7-4.6S45.8,36.4,45.8,39z"/>
+        	<path class="st0" d="M73.5,32.2l-3.8,13.6H67l-2.1-10.4l-2.1,10.4h-2.7l-3.8-13.6h2.9l2.3,9.6l2-9.6h2.9l1.9,9.6l2-8.5
+        		c0.2-0.7,0.7-1.1,1.4-1.1C71.7,32.2,73.5,32.2,73.5,32.2z"/>
+        	<path class="st0" d="M82.5,42c0-0.7-0.4-1.1-1.2-1.3c-0.8-0.3-1.7-0.5-2.7-0.7c-2.6-0.7-4-1.9-4-3.7c0.1-2.9,1.7-4.3,5.1-4.2
+        		c3.2,0,4.8,1.4,5,4.2h-1.5c-0.6,0-1-0.3-1.2-0.8c-0.4-0.8-1.3-1.1-2.4-1.1c-1.4,0-2.1,0.6-2.2,1.6c0,0.9,0.8,1.5,2.6,1.9
+        		c1,0.3,2.1,0.5,3.2,1c1.5,0.5,2.2,1.6,2.1,3.4c-0.2,2.7-1.9,4-5.4,4c-3.6,0-5.4-1.5-5.5-4.5h2.7c0.1,1.4,1,2.2,2.8,2.2
+        		C81.7,43.7,82.5,43.1,82.5,42z"/>
+        	<path class="st0" d="M88,32.2h1.4v5.1h5.5v-5.1h2.8v13.6h-2.8v-6h-5.5v6h-2.8V33.7C86.6,32.9,87.2,32.2,88,32.2z"/>
+        	<path class="st0" d="M99,33.7c0-1,0.4-1.5,1.4-1.5h1.4v13.6h-2.7L99,33.7L99,33.7z"/>
+        	<path class="st0" d="M109.3,41h-3.4v4.9h-2.7V33.7c0-0.8,0.6-1.5,1.4-1.5h4.6c2.9,0,4.3,1.5,4.3,4.4C113.5,39.3,112,41,109.3,41z
+        		 M108.6,38.5c1.4,0,2.1-0.6,2.1-2s-0.6-2-1.9-2H106v3.9L108.6,38.5L108.6,38.5z"/>
+        </g>
+        </svg>
+      </a>
+
+        <nav class="nav-primary">
+          <?php
+          if (has_nav_menu('primary_navigation')) {
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          }
+          ?>
+        </nav>
+        <div class="banner__hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <?php
+        if (has_nav_menu('secondary_navigation')) {
+        ?>
+        <nav class="nav-secondary">
+          <p class="nav-secondary__title"><a href="<?php bloginfo('url') ?>"><?php bloginfo('name'); ?></a></p>
+          <?php wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'nav']); ?>
+        </nav>
+        <?php } ?>
+
+    </header>
+
+    <?php
+    if (has_post_thumbnail()) {
+      $thumb_id = get_post_thumbnail_id();
+      $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+      $thumb_url = $thumb_url_array[0];
+    ?>
+
+      <div class="thf__page-header parallax-window" data-parallax="scroll" data-speed="0.2" data-image-src="<?php echo $thumb_url ?>">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    <?php } else { ?>
+      <div class="thf__page-header no-photo">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    <?php } ?>
+
+    <div class="wrap container" role="document">
+      <div class="content">
+        <main class="main">
+          <?php do_action('eventon_single_content_wrapper'); ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+              <article <?php post_class() ?>>
+                <div class="entry-content">
+                  <?php do_action('eventon_single_content'); ?>
+                </div>
+              </article>
+          <?php endwhile; ?>
+          <?php do_action('eventon_single_after_loop'); ?>
+        </main>
+      </div>
+    </div>
+    <?php do_action('get_footer'); ?>
+    <?php //do_action('eventon_after_main_content'); ?>
+    <footer class="content-info">
+      <a href="/" class="footer__logo">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/thflogosquarewhite.svg'; ?>" alt="The Heights Fellowship logo white">
+      </a>
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+      <div class="footer__social">
+        <a href="http://www.facebook.com/theheightsfellowship" class="footer-social__icon facebook"></a>
+        <a href="http://www.twitter.com/thflubbock" class="footer-social__icon twitter"></a>
+        <a href="http://instagram.com/thflubbock" class="footer-social__icon instagram"></a>
+        <a href="http://www.youtube.com/user/TheHeightsFellowship" class="footer-social__icon youtube"></a>
+        <a href="https://itunes.apple.com/us/app/heights-fellowship-lubbock/id424692506?mt=8" class="footer-social__icon itunes"></a>
+      </div>
+      <p class="copyright">&copy; The Heights Fellowship <?php echo date('Y') ?></p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
